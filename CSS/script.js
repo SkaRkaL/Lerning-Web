d6e7fdd699500664f2d4a33eb3090072b0a977fb
sakarkal <karkalsaad@gmail.com>
2024-02-13 21:03:34 +0100

    fixxing

--- CSS/script.js
+++ CSS/script.js
@@ -14,38 +14,3 @@ var bubblyButtons = document.getElementsByClassName("bubbly-button");
 for (var i = 0; i < bubblyButtons.length; i++) {
 	bubblyButtons[i].addEventListener('click', animateButton, false);
 }
-
-// JavaScript to simulate the typing animation
-const strings = [
-	"SaKarKal",
-	"Sma9ma9",
-	"And i am a ",
-	"Frontend Developer",
-	"Software Engineer",
-];
-const textElement = document.querySelector('.text-accent');
-let stringIndex = 0;
-let charIndex = 0;
-
-function type() {
-	if (charIndex < strings[stringIndex].length) {
-		textElement.textContent += strings[stringIndex].charAt(charIndex);
-		charIndex++;
-		setTimeout(type, 50);
-	} else {
-		setTimeout(erase, 2000);
-	}
-}
-
-function erase() {
-	if (charIndex > 0) {
-		textElement.textContent = strings[stringIndex].substring(0, charIndex - 1);
-		charIndex--;
-		setTimeout(erase, 50);
-	} else {
-		stringIndex = (stringIndex + 1) % strings.length;
-		setTimeout(type, 1000);
-	}
-}
-
-type(); // Start the typing animation
